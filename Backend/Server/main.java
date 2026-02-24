7f33b633d8635b8905662d86e4517157b1f9a61c
Bailie <byrnebailie@gmail.com>
2026-02-24 15:04:44 +0000

    merged with leos client

--- Backend/Server/main.java
+++ Backend/Server/main.java
@@ -5,31 +5,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class main extends Application {
+public class main {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        System.out.println("JavaFX is working!");
-        loadFXML("loginmenu.fxml");
-    }
-
-    public static void loadFXML(String fxmlFile) {
-        try {
-            java.net.URL resource = main.class.getResource(fxmlFile);
-            if (resource == null) {
-                System.err.println("FXML file not found: " + fxmlFile);
-                return;
-            }
-            FXMLLoader loader = new FXMLLoader(resource);
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Starting server...");
     }
 }
